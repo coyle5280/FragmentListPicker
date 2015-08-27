abc5e13f563bbfd090bd318340524022f9e0a4af
Joshua Coyle <coyle5280@gmail.com>
2015-08-26 21:58:55 -0600

    A Start to Assignment Two.  Setup of main components including fragments one and two.java, mainActivity.java, and corresponding xml files.

---
+++ FragmentListPicker/src/Mobile/App/Assignment2/FragmentTwo.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Mobile.App.Assignment2;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.LayoutInflater;
+
+/**
+ *
+ * @author godzilla
+ */
+public class FragmentTwo extends Fragment {
+    
+    public View onCreate(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        
+        
+        
+        return inflater.inflate(R.layout.frag2, container, false);
+    }
+    
+    
+}
